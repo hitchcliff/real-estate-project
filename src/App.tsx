bd0eb7f1c4476b5d8ae5bf86ec5889d33b9525e0
Kevin Nacario <hitchcliff1@gmail.com>
2020-09-18 22:53:17 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import './App.scss';
 import { RealEstate } from './components';
-=======
-import logo from './logo.svg';
-import './App.css';
->>>>>>> parent of c1b17fe... first commit
-=======
-import logo from './logo.svg';
-import './App.css';
->>>>>>> parent of c1b17fe... first commit
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <RealEstate/>    
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-<<<<<<< HEAD
->>>>>>> parent of c1b17fe... first commit
-=======
->>>>>>> parent of c1b17fe... first commit
     </div>
   );
 }
