66001093c625c00aa41499f0abd455e5a3ff7bc7
kevin nacario <hitchcliff1@gmail.com>
2020-10-08 20:45:09 +0800

    added property History

--- src/components/PropertyHistory/PropertyHistory.tsx
+++ src/components/PropertyHistory/PropertyHistory.tsx
@@ -1,7 +1,53 @@
+import { faHistory } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import styles from './PropertyHistory.module.scss';
-const PropertyHistory = () => {
-  return <div className={styles.container}></div>;
+interface IPropertyHistoryProp<T> {
+  item: T[];
+}
+const PropertyHistory = <T,>({ item }: IPropertyHistoryProp<T>) => {
+  if (!item) return <></>;
+  console.log(item);
+  const mapHistory = item.map((property: T | any, index) => {
+    if (!property) return;
+
+    const { date, event_name, price, source } = property;
+    return (
+      <tr key={index}>
+        <td>{new Date(date).toLocaleDateString()}</td>
+        <td>{event_name}</td>
+        <td>{price}</td>
+        <td>{source}</td>
+      </tr>
+    );
+  });
+  return (
+    <div className={styles.container}>
+      <div className={styles.heading}>
+        <FontAwesomeIcon icon={faHistory} />
+        Property History
+      </div>
+      <div className={styles.body}>
+        <table>
+          <tbody>
+            <tr>
+              <th>Date</th>
+              <th>Event</th>
+              <th>Price</th>
+              <th>Source</th>
+            </tr>
+            {mapHistory}
+          </tbody>
+        </table>
+      </div>
+      <div className={styles.footer}>
+        About History &amp; Taxes Data The price and tax history data displayed is
+        obtained from public records and/or MLS feeds from the local jurisdiction. Contact
+        your REALTOR® directly in order to obtain the most up-to-date information
+        available.
+      </div>
+    </div>
+  );
 };
 
 export default PropertyHistory;
