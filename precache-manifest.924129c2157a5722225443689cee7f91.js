7bcada98a5cb15a9b2c48632e00a47a16a2a4308
kevin nacario <hitchcliff1@gmail.com>
2020-10-31 23:15:36 +0800

    Updates

---
+++ precache-manifest.924129c2157a5722225443689cee7f91.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d560ef49ab0d2e8fa515814985d91c",
+    "url": "/real-estate-project/index.html"
+  },
+  {
+    "revision": "f76d6c6290f5ae1930e4",
+    "url": "/real-estate-project/static/css/2.515daa14.chunk.css"
+  },
+  {
+    "revision": "be8b84d8989d93d11538",
+    "url": "/real-estate-project/static/css/main.00b60ef2.chunk.css"
+  },
+  {
+    "revision": "f76d6c6290f5ae1930e4",
+    "url": "/real-estate-project/static/js/2.f1d9111f.chunk.js"
+  },
+  {
+    "revision": "947451fe8565b2294253db05c7bccd60",
+    "url": "/real-estate-project/static/js/2.f1d9111f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be8b84d8989d93d11538",
+    "url": "/real-estate-project/static/js/main.fb058b7b.chunk.js"
+  },
+  {
+    "revision": "743578b8f9cc89282b0e",
+    "url": "/real-estate-project/static/js/runtime-main.d8072d03.js"
+  },
+  {
+    "revision": "014865449d0f1e3a9d793c7a49509fb9",
+    "url": "/real-estate-project/static/media/logo.01486544.svg"
+  },
+  {
+    "revision": "ed985fc5d2fcca807984e02ab2dc02ae",
+    "url": "/real-estate-project/static/media/urban_slums.ed985fc5.jpg"
+  }
+]);
